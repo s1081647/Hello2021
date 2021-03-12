b2ee92c88fbe480495e51cb00821710d778fd949
student <s1081647@gm.pu.edu.tw>
2021-03-12 15:58:11 +0800

    第一次commit

--- app/src/main/java/tw/edu/pu/csim/s1081647/hello/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/s1081647/hello/MainActivity.kt
@@ -7,5 +7,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var txv: TextView=findViewById<TextView>(R.id.txv)
+        txv.test="嗨，我是游騰信"
+    }
+    fun TestMaster(){
+        var txv: TextView=finfViewById<TextView>(R.id.txv)
+        txv.text="Master分支"
     }
 }
